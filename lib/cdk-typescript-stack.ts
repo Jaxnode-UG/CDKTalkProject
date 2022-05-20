cdf0ad23490e4ee13fb53392424dd81b84123302
willa75 <anthonywi99@yahoo.com>
2022-05-20 16:03:05 -0400

    Added comments to the stack

--- lib/cdk-typescript-stack.ts
+++ lib/cdk-typescript-stack.ts
@@ -18,6 +18,7 @@ export class CdkTypescriptStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // dynamodb table
     const postsTable = new dynamodb.Table(this, 'PostsTable', {
       partitionKey: {name: 'id', type: dynamodb.AttributeType.STRING},
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
@@ -25,6 +26,7 @@ export class CdkTypescriptStack extends Stack {
       removalPolicy: RemovalPolicy.DESTROY // destroys table when removed from stack
     });
 
+    // Define a lambda
     const getListLambda = new lambda.Function(this, 'CdkListFunction', {
       runtime: lambda.Runtime.NODEJS_16_X,
       code: lambda.Code.fromAsset('lambdas'),
@@ -34,8 +36,10 @@ export class CdkTypescriptStack extends Stack {
       }
     });
 
+    // give lambda permission to read data in the dynamodb table
     postsTable.grantReadData(getListLambda);
 
+    // created a userpool that allows users to sign in with their email
     const userPool = new cognito.UserPool(this, 'CdkAuth', {
       userPoolName: 'CdkAuth',
       selfSignUpEnabled: true,
@@ -44,6 +48,7 @@ export class CdkTypescriptStack extends Stack {
       }
     });
 
+    // added a web client for our userpool, necessary for AppSync
     const webClient = userPool.addClient('web', {
       userPoolClientName: 'web',
       authFlows: {
@@ -52,6 +57,7 @@ export class CdkTypescriptStack extends Stack {
       }
     });
 
+    // AppSync api configured to use our cognito for authentication, logs, and xray enabled
     const api = new appsync.GraphqlApi(this, 'CdkApi', {
       name: 'cdk-api',
       schema: appsync.Schema.fromAsset('schema.api.graphql'),
@@ -70,10 +76,11 @@ export class CdkTypescriptStack extends Stack {
       xrayEnabled: true,
     });
 
+    // a DataSource for the created dynamodb table and lamdba function
     const postsDS = api.addDynamoDbDataSource('postsTable', postsTable);
     const getListFunctionDS = api.addLambdaDataSource('getListFunction', getListLambda);
 
-    // get list of posts with a lambda
+    // get list of posts with a lambda datasource
     getListFunctionDS.createResolver({
       typeName: 'Query',
       fieldName: 'getPosts'
@@ -87,7 +94,7 @@ export class CdkTypescriptStack extends Stack {
     //   responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultList()
     // });
 
-    // create post
+    // create post using Dynamo DataSource
     postsDS.createResolver({
       typeName: 'Mutation',
       fieldName: 'createPost',
@@ -98,10 +105,12 @@ export class CdkTypescriptStack extends Stack {
     // create a vpc, subnets, and natgateways
     const vpc = new ec2.Vpc(this, 'VpcForFargate');
 
+    // create a cluster using the created vpc
     const cluster = new ecs.Cluster(this, "EcsClusterForFargate", {
       vpc
     });
 
+    // create a fargate service
     const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "CdkFargateService", {
       cluster,
       cpu: 256,
@@ -111,6 +120,7 @@ export class CdkTypescriptStack extends Stack {
       }
     });
 
+    // output the url for the AppSync Api
     new CfnOutput(this, 'GraphQL_URL', { value: api.graphqlUrl });
     
   }
